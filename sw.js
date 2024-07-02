745e34d69d253be8efcca2c5d3bb9b5a072187e2
mfro-yoshikiokada <yoshiki.okada@mfro.net>
2024-07-02 17:59:35 +0900

    a

---
+++ sw.js
@@ -0,0 +1,30 @@
+// キャッシュ名
+var CACHE_NAME = 'pwa-test-caches';
+
+// キャッシュするファイル
+var urlsToCache = [
+	'index.html',
+	'style.css',
+];
+
+// インストール処理
+self.addEventListener('install', function(event) {
+	event.waitUntil(
+		caches
+			.open(CACHE_NAME)
+			.then(function(cache) {
+				return cache.addAll(urlsToCache);
+			})
+	);
+});
+
+// リソースフェッチ時のキャッシュロード処理
+self.addEventListener('fetch', function(event) {
+	event.respondWith(
+		caches
+			.match(event.request)
+			.then(function(response) {
+				return response ? response : fetch(event.request);
+			})
+	);
+});
